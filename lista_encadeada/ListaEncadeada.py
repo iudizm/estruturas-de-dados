ba3eec8ab73e838f05d7f637c085da6f1380b56a
iudi <iudizm@pm.me>
2022-09-21 21:05:25 -0300

    Implementa metodos restantes de lista

--- lista_encadeada/ListaEncadeada.py
+++ lista_encadeada/ListaEncadeada.py
@@ -5,21 +5,23 @@ class ListaEncadeada():
         self.__cabeca = None
         self.__tamanho = 0
 
-    def ultimo_elemento(self):
-        elemento = self.__cabeca
-        for i in range(0, self.tamanho()):
-            elemento = elemento.proximo()
-            i += 1
-        return elemento
+    def acessa_ultimo(self):
+        if not self.esta_vazia():
+            elemento = self.__cabeca
+            for i in range(0, self.tamanho()):
+                elemento = elemento.proximo()
+            return elemento
+        return "lista vazia"
 
-# -------
+    def acessa_primeiro(self):
+        return self.__cabeca.info()
+
     def clear(self):
         pass
 
-# -------
     def insere(self, info, index):
         novo_elemento = Elemento(info)
-        if self.__tamanho == 0:
+        if self.esta_vazia():
         	self.__cabeca = novo_elemento
         else:
 	        if index == 0:
@@ -39,21 +41,38 @@ class ListaEncadeada():
     def insere_no_comeco(self, info):
         self.insere(info, 0)
 
-# -------
-    def insere_ordenado(self, info):
-        pass
+    def insere_antes_de(self, referencia, info):
+        self.insere(info, self.busca_posicao_por_info(referencia))
 
-# -------
+    def insere_depois_de(self, referencia, info):
+        self.insere(info, (self.busca_posicao_por_info(referencia) + 1))
+
     def busca_info_por_posicao(self, index):
-        pass
+        if not self.esta_vazia():
+            elemento = self.__cabeca
+            for i in range(0, index):
+                elemento = elemento.proximo()
+            return elemento.info()
+        return "lista vazia"
 
-# -------
     def busca_posicao_por_info(self, info):
-        pass
+        elemento = self.__cabeca
+        for i in range(0, self.tamanho()):
+            if elemento.info() == info:
+                print("encontrado em indice:" + str(i) + "o valor" + info)
+                return i
+            elemento = elemento.proximo()
+        return None
 
-# -------
     def pop(self, index):
-        pass
+    	if not self.lista_vazia():
+    		anterior = self.__cabeca
+    		for i in range(1, index):
+    			anterior = anterior.proximo()
+    		info = anterior.proximo.info()
+    		anterior.set_proximo(anterior.proximo().proximo())
+    		return info
+    	return False
 
     def pop_do_fim(self):
         self.pop(self.tamanho() - 1)
@@ -85,7 +104,7 @@ class ListaEncadeada():
 	    		c = c.proximo()
 	    	print(array)
     	else:
-    		print("lista vazia")	
+    		print("lista vazia")
 
 lista = ListaEncadeada()
 lista.imprime()
@@ -99,4 +118,11 @@ lista.insere_no_comeco("oi no começooo")
 lista.imprime()
 print("tamanho: " + str(lista.tamanho()))
 print("head: " + lista.cabeca().info())
+lista.imprime()
+
+lista.insere_antes_de("oi no começooo","AAAAAAAA")
+lista.imprime()
+lista.insere_depois_de("AAAAAAAA", "BBBB")
+lista.imprime()
+lista.insere_antes_de("AAAAAAAA", "000")
 lista.imprime()
